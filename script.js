a0b2bd234bc40275b88c9bd164be974c8a2730f4
Davronbekn202 <davronbekn202@gmail.com>
2026-02-16 23:57:13 +0500

    JS test

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// let user = {
+//     name: "Davronbek",
+//     age: 20
+// };
+// console.log(user['age']);
+// let i = 3
+// // for(i;i<10; i++){
+// //     console.log('salom')
+// // }
+// for (let key in user) {
+//     console.log(key, user[key]);
+// }
+
+// if (i > 5)
+// {
+//     console.log('i 5 dan katta');
+// }else if (i == 5){
+//     console.log('i 5 ga teng');
+// }else 
+//     console.log('i 5 dan kichik');
+
+
+const arr = [1, 2, 3, 4, 5];
+const arr1 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+const new2 = arr.concat(arr1)
+console.log(new2);
